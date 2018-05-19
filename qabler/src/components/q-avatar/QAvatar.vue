0b7ec0137e957f5aba8b0ea3bf3d935028beab28
QianGuoqing <qian_guoqing@yeah.net>
2018-05-19 12:33:37 +0800

    添加header第一部分

---
+++ qabler/src/components/q-avatar/QAvatar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="q-avatar">
+    <slot name="avatar"></slot>
+    <div class="info">
+      <div class="title">{{ title }}</div>
+      <div class="sub-title">{{ subTitle }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'QAvatar',
+    props: {
+      title: {
+        type: String,
+        default: ''
+      },
+      subTitle: {
+        type: String,
+        default: ''
+      }
+    },
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .q-avatar
+    display flex
+    .info
+      margin-left 0.5rem
+      .title
+        font-size 0.9375rem
+        font-weight 400
+        color #495057
+      .sub-title
+        color #9aa0ac
+        margin-top 0.25rem
+        font-size 87.5%
+</style>
--- qabler/src/views/q-header/QHeader.vue
+++ qabler/src/views/q-header/QHeader.vue
@@ -9,7 +9,19 @@
               <img src="../../assets/images/brand/tabler.svg" class="logo" alt="">
             </div>
             <div class="info-wrapper">
-              aaa
+              <div class="source-button">source code</div>
+              <div class="header-badge">
+                <a-badge dot>
+                  <a-icon type="notification"/>
+                </a-badge>
+              </div>
+              <div class="header-avatar">
+                <q-avatar title="Jane Pearson" sub-title="Administrator">
+                  <div slot="avatar" class="avatar-wrapper">
+                    <img src="../../assets/images/faces/female/19.jpg" alt="" class="avatar">
+                  </div>
+                </q-avatar>
+              </div>
             </div>
           </div>
         </a-col>
@@ -29,14 +41,20 @@
 </template>
 
 <script>
+  import QAvatar from '../../components/q-avatar/QAvatar.vue'
   export default {
-    name: 'QHeader'
+    name: 'QHeader',
+    components: {
+      QAvatar
+    }
   }
 </script>
 
 <style lang="stylus" scoped>
   .header
     .header-info
+      padding 1rem 0
+      border-bottom 1px solid rgba(0, 40, 100, 0.12)
       .header-content
         display flex
         justify-content space-between
@@ -44,4 +62,48 @@
           display flex
           .logo
             height 2rem
+            transition all .3s
+            cursor pointer
+            &:hover
+              opacity 0.8
+        .info-wrapper
+          display flex
+          align-items center
+          .source-button
+            display inline-block
+            font-size 0.75rem
+            font-weight 600
+            letter-spacing .03rem
+            padding 0.25rem 0.5rem
+            border-radius 3px
+            line-height 1.333333
+            color #467fcf
+            border 1px solid #467fcf
+            cursor pointer
+            transition all .3s
+            &:hover
+              color #fff
+              background-color #467fcf
+          .header-badge
+            color #333
+            width 40px
+            height 35px
+            text-align center
+            line-height 35px
+            margin 0 .5rem
+            cursor pointer
+            transition all .3s
+            :hover
+              color #495057
+          .header-avatar
+            cursor pointer
+            transition all .3s
+            &:hover
+              text-shadow 0 0 2px #ccc
+            .avatar-wrapper
+              .avatar
+                width 2rem
+                height 2rem
+                line-height 2rem
+                border-radius 50%
 </style>
